1b8f884ca85a79dc568ab4e4add3685bba726bf2
Oscar Boykin <oscar@twitter.com>
2013-03-26 08:35:27 -0700

    Uses dense matrix code, doesn't work yet

--- src/main/scala/org/zymnis/scalafish/SimpleMatrixFactorizer.scala
+++ src/main/scala/org/zymnis/scalafish/SimpleMatrixFactorizer.scala
@@ -1,43 +1,83 @@
 package org.zymnis.scalafish
 
-import breeze.linalg._
+import org.zymnis.scalafish.matrix._
+
+import Syntax._ // Matrix syntax
 
 import scala.util.Random
 
+
 object SimpleMatrixFactorizer extends App {
-  val (real, data) = MatrixUtil.randSparseMatrix(200, 100, 10, 0.10, 0.001)
-  val mf = new SimpleMatrixFactorizer(data, 15, 1e-3, 1e-2)
+  val (rows, cols, rank) = (200, 100, 10)
+  val real = DenseMatrix.randLowRank(rows, cols, rank)
+  val data = SparseMatrix.sample(0.1, real)
+  val approx = DenseMatrix.zeros(rows, cols) // probably don't materialize this in the real deal
+
+  val mf = new SimpleMatrixFactorizer(data, 15, 1e-3f, 1e-2f)
   (0 to 20).foreach{ i =>
     val obj = mf.currentObjective
-    val relerr = math.sqrt(MatrixUtil.frobNorm(mf.currentGuess - real)) / math.sqrt(MatrixUtil.frobNorm(real))
+    approx := mf.currentGuess
+    approx := approx - real
+    val relerr = math.sqrt(Matrix.frobNorm2(approx)) / math.sqrt(Matrix.frobNorm2(real))
     println("iteration: " + i + ", obj: " + obj + ", relerr: " + relerr)
     mf.update
   }
+  println(approx)
+  println(mf.L)
 }
 
-class SimpleMatrixFactorizer(data: CSCMatrix[Double], rank: Int,
-    mu: Double, alpha: Double) {
+class SimpleMatrixFactorizer(data: SparseMatrix, rank: Int, mu: Float, alpha: Float) {
 
   val L = DenseMatrix.rand(data.rows, rank)
   val R = DenseMatrix.rand(data.cols, rank)
 
   var iteration = 1
 
-  // Get the sparsity pattern in a separate matrix
-  val pat = DenseMatrix.zeros[Double](data.rows, data.cols)
-  data.activeKeysIterator.foreach{ case(r, c) => pat(r, c) = 1.0 }
+  private var currentDelta = SparseMatrix.zeros(data.rows, data.cols)
 
-  def currentObjective: Double =
-    0.5 * (MatrixUtil.frobNorm(currentDelta) + mu * (MatrixUtil.frobNorm(L) + MatrixUtil.frobNorm(R)))
+  var currentObjective: Double = computeObjective
 
-  def currentDelta = pat :* (L*R.t - data)
+  def computeObjective: Double =
+    0.5 * (Matrix.frobNorm2(currentDelta) + mu * (Matrix.frobNorm2(L) + Matrix.frobNorm2(R)))
+
+  def delta: MatrixUpdater = new MatrixUpdater {
+    def update(oldDelta: Matrix) = {
+      val iter = data.denseIndices
+      while(iter.hasNext) {
+        val idx = iter.next
+        val row = data.indexer.row(idx)
+        val col = data.indexer.col(idx)
+        // going to set: oldDelta(row, col) = L(row,_) * R(col,_) - data(row, col)
+        var rankIdx = 0
+        var sum = 0.0
+        while(rankIdx < rank) {
+          sum += L(row, rankIdx) * R(col, rankIdx)
+          rankIdx += 1
+        }
+        oldDelta.update(row, col, (sum - data(idx)).toFloat)
+      }
+    }
+  }
 
   def update {
     val newAlpha = alpha / iteration
-    L := L - (L * mu + currentDelta * R) * newAlpha
-    R := R - (R * mu + currentDelta.t * L) * newAlpha
+
+    currentDelta := delta
+    currentDelta *= newAlpha
+
+    L *= (1.0f - mu * newAlpha)
+    L += (currentDelta * R)
+
+    currentDelta := delta
+    // Slightly out of date by the end, but cheaper
+    currentObjective = computeObjective
+    currentDelta *= newAlpha
+
+    R *= (1.0f - mu * newAlpha)
+    R += (currentDelta.t * L)
+
     iteration += 1
   }
 
-  def currentGuess = L * R.t
+  def currentGuess: MatrixUpdater = L * R.t
 }
