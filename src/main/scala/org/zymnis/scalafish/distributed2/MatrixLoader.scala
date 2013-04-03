c1130fe1796783095bff44119a7b67926fc4cae6
Oscar Boykin <oscar@twitter.com>
2013-04-02 21:53:32 -0700

    Adds MatixWriter, unused

--- src/main/scala/org/zymnis/scalafish/distributed2/MatrixLoader.scala
+++ src/main/scala/org/zymnis/scalafish/distributed2/MatrixLoader.scala
@@ -28,4 +28,30 @@ class TestLoader extends MatrixLoader {
   def rowPartition(parts: Int) = load.rowSlice(parts).map { MatrixLoader.from(_) }
 }
 
+trait MatrixWriter {
+  def rows: Int
+  def cols: Int
+  /**
+   * This produces an IndexedSeq exactly parts in length,
+   * calling
+   * {{{
+   *
+   * matrix.rowSlice(parts)
+   *   .zip(writer.rowPartition(parts))
+   *   .foreach { case (m,mw) => mw.write(m) }
+   *
+   * }}}
+   * should be the same as writer.write(matrix)
+   */
+  def rowPartition(parts: Int): IndexedSeq[MatrixWriter]
+  def write(m: Matrix): Unit
+}
 
+class PrintWriter(override val rows: Int, override val cols: Int) extends MatrixWriter {
+  def write(m: Matrix) = {
+    require(m.rows == rows, "Rows don't match %d != %d".format(m.rows, rows))
+    require(m.cols == cols, "Cols don't match %d != %d".format(m.cols, cols))
+    println(m)
+  }
+  def rowPartition(parts: Int) = sys.error("Not yet implemented")
+}
